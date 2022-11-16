852674b76c1fae1bb7b285fc1fa2f3595b6f1d9e
Geert-Jan Zwiers <geertjan.zwiers@nslogin.nl>
2022-11-16 11:04:29 +0000

    Merged PR 104023: fix: working dirs
    fix: working dirs

--- rest-api-public/typescript/cdk/bin/rest-api-typescript.ts
+++ rest-api-public/typescript/cdk/bin/rest-api-typescript.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
+import { BuildConfig, Config } from '@ns/cdk-helper';
 import * as cdk from 'aws-cdk-lib';
-import {BuildConfig, Config} from '@ns/cdk-helper';
-import {ApiStack} from '../lib/api-stack';
+import { ApiStack } from '../lib/api-stack';
 
 const app = new cdk.App();
 
 // NS Specific Config helper: https://dev.azure.com/ns-topaas/NSCAWS/_git/ns-shared-cdk-helper
 const buildConfig: BuildConfig = Config(app);
 
-let apiStack = new ApiStack(app, 'RestOpenApiTypescriptStack', {
-    env: {
-        region: buildConfig.AWSRegion,
-        account: buildConfig.AWSAccount,
-    },
-    buildConfig: buildConfig,
-    tags: buildConfig.Tags,
+new ApiStack(app, 'RestOpenApiTypescriptStack', {
+  env: {
+    region: buildConfig.AWSRegion,
+    account: buildConfig.AWSAccount,
+  },
+  buildConfig: buildConfig,
+  tags: buildConfig.Tags,
 });
--- rest-api-public/typescript/cdk/lib/api-stack.ts
+++ rest-api-public/typescript/cdk/lib/api-stack.ts
@@ -1,110 +1,110 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { BuildConfig } from '@ns/cdk-helper';
 import * as cdk from 'aws-cdk-lib';
-import {RemovalPolicy, StackProps} from 'aws-cdk-lib';
-import {Construct} from 'constructs';
+import { RemovalPolicy, StackProps } from 'aws-cdk-lib';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import {LogGroupLogDestination} from 'aws-cdk-lib/aws-apigateway';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import {Architecture} from 'aws-cdk-lib/aws-lambda';
+import { LogGroupLogDestination } from 'aws-cdk-lib/aws-apigateway';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import {BillingMode, Table} from 'aws-cdk-lib/aws-dynamodb';
-import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
-import * as cloudwatch from "aws-cdk-lib/aws-logs";
-import {RetentionDays} from "aws-cdk-lib/aws-logs";
-import {ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import { BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Architecture } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as cloudwatch from 'aws-cdk-lib/aws-logs';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Construct } from 'constructs';
 
-import * as path from "path";
 import * as Mustache from 'mustache';
-import * as fs from "fs";
-import {BuildConfig} from "@ns/cdk-helper";
 
 export class ApiStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: ApiStackConfigProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props: ApiStackConfigProps) {
+    super(scope, id, props);
 
-        const ddb = this.createDatabase(props)
+    const ddb = this.createDatabase(props);
 
-        const listLambda = this.createFunction('ListArtists', 'list-artists.ts', ddb.tableName, props)
-        const getLambda = this.createFunction('GetArtist', 'get-artist.ts', ddb.tableName, props)
-        const createLambda = this.createFunction('CreateArtist', 'create-artist.ts', ddb.tableName, props)
+    const listLambda = this.createFunction('ListArtists', 'list-artists.ts', ddb.tableName, props);
+    const getLambda = this.createFunction('GetArtist', 'get-artist.ts', ddb.tableName, props);
+    const createLambda = this.createFunction('CreateArtist', 'create-artist.ts', ddb.tableName, props);
 
-        ddb.grantReadData(getLambda)
-        ddb.grantReadData(listLambda);
-        ddb.grantReadWriteData(createLambda)
+    ddb.grantReadData(getLambda);
+    ddb.grantReadData(listLambda);
+    ddb.grantReadWriteData(createLambda);
 
-        this.createRestApi(props, listLambda.functionArn, getLambda.functionArn, createLambda.functionArn);
+    this.createRestApi(props, listLambda.functionArn, getLambda.functionArn, createLambda.functionArn);
+  }
+
+  private createDatabase(props: ApiStackConfigProps): Table {
+    return new dynamodb.Table(this, 'ArtistTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      pointInTimeRecovery: true,
+      removalPolicy: props.buildConfig.Environment == 'Dev' ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
+    });
+  }
+
+  private createRestApi(props: ApiStackConfigProps, listArn: string, getArn: string, createArn: string) {
+    const apigwLogGroup = new cloudwatch.LogGroup(this, 'ApiGWLogs', {
+      retention: RetentionDays.ONE_WEEK,
+      removalPolicy: props.buildConfig.Environment == 'Dev' ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
+    });
+
+    let api = new apigateway.SpecRestApi(this, 'ArtistsApi', {
+      apiDefinition: apigateway.ApiDefinition.fromInline(
+        this.generateOpenApiSpec({
+          ListArtistsLambda: listArn,
+          CreateArtistLambda: createArn,
+          GetArtistLambda: getArn,
+        })),
+      deployOptions: {
+        loggingLevel: apigateway.MethodLoggingLevel.ERROR,
+        accessLogDestination: new LogGroupLogDestination(apigwLogGroup),
+        metricsEnabled: true,
+        tracingEnabled: true,
+      },
+    });
+
+    if (props.buildConfig.Parameters != null && props.buildConfig.Parameters.Domain != null) {
+      new apigateway.BasePathMapping(this, 'MyBasePathMapping', {
+        restApi: api,
+        domainName: props.buildConfig.Parameters.Domain,
+        basePath: props.buildConfig.Parameters.BasePath,
+      });
     }
+  }
 
-    private createDatabase(props: ApiStackConfigProps): Table {
-        return new dynamodb.Table(this, "ArtistTable", {
-            partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING},
-            billingMode: BillingMode.PAY_PER_REQUEST,
-            pointInTimeRecovery: true,
-            removalPolicy: props.buildConfig.Environment == 'Dev' ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN
-        });
-    }
+  private createFunction(id: string, handler: string, tableName: string, props: ApiStackConfigProps): lambda.Function {
+    let apiLambda = new NodejsFunction(this, id, {
+      entry: path.join(__dirname, '../../app/' + handler),
+      runtime: lambda.Runtime.NODEJS_16_X,
+      architecture: Architecture.ARM_64,
+      // Increased memory to improve cold starts
+      memorySize: 1024,
+      handler: 'handler',
+      bundling: {
+        minify: true,
+      },
+      // TODO: Setup logging retention to your team/app requirements but don't forget to set it (=infinite)
+      logRetention: RetentionDays.ONE_MONTH,
+      tracing: lambda.Tracing.ACTIVE,
+      environment: {
+        ARTIST_TABLE: tableName,
+        POWERTOOLS_SERVICE_NAME: props.buildConfig.App,
+      },
+    });
 
-    private createRestApi(props: ApiStackConfigProps, listArn: string, getArn: string, createArn: string) {
-        const apigwLogGroup = new cloudwatch.LogGroup(this, 'ApiGWLogs', {
-            retention: RetentionDays.ONE_WEEK,
-            removalPolicy: props.buildConfig.Environment == 'Dev' ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
-        })
+    const apigwServicePrincipal = new ServicePrincipal('apigateway.amazonaws.com');
+    apiLambda.grantInvoke(apigwServicePrincipal);
 
-        let api = new apigateway.SpecRestApi(this, 'ArtistsApi', {
-            apiDefinition: apigateway.ApiDefinition.fromInline(
-                this.generateOpenApiSpec({
-                    'ListArtistsLambda': listArn,
-                    'CreateArtistLambda': createArn,
-                    'GetArtistLambda': getArn
-                })),
-            deployOptions: {
-                loggingLevel: apigateway.MethodLoggingLevel.ERROR,
-                accessLogDestination: new LogGroupLogDestination(apigwLogGroup),
-                metricsEnabled: true,
-                tracingEnabled: true
-            },
-        });
+    return apiLambda;
+  }
 
-        if (props.buildConfig.Parameters != null && props.buildConfig.Parameters.Domain != null) {
-            new apigateway.BasePathMapping(this, 'MyBasePathMapping', {
-                restApi: api,
-                domainName: props.buildConfig.Parameters.Domain,
-                basePath: props.buildConfig.Parameters.BasePath,
-            });
-        }
-    }
-
-    private createFunction(id: string, handler: string, tableName: string, props: ApiStackConfigProps): lambda.Function {
-        let apiLambda = new NodejsFunction(this, id, {
-            entry: path.join(__dirname, '../../app/' + handler),
-            runtime: lambda.Runtime.NODEJS_16_X,
-            architecture: Architecture.ARM_64,
-            // Increased memory to improve cold starts
-            memorySize: 1024,
-            handler: 'handler',
-            bundling: {
-                minify: true
-            },
-            // TODO: Setup logging retention to your team/app requirements but don't forget to set it (=infinite)
-            logRetention: RetentionDays.ONE_MONTH,
-            tracing: lambda.Tracing.ACTIVE,
-            environment: {
-                ARTIST_TABLE: tableName,
-                POWERTOOLS_SERVICE_NAME: props.buildConfig.App,
-            }
-        });
-
-        const apigwServicePrincipal = new ServicePrincipal('apigateway.amazonaws.com');
-        apiLambda.grantInvoke(apigwServicePrincipal)
-
-        return apiLambda;
-    }
-
-    private generateOpenApiSpec(vars: any) {
-        return this.resolve(Mustache.render(
-            fs.readFileSync(path.join(__dirname, '../../openapi.yaml'), 'utf-8'), vars));
-    }
+  private generateOpenApiSpec(vars: any) {
+    return this.resolve(Mustache.render(
+      fs.readFileSync(path.join(__dirname, '../../openapi.yaml'), 'utf-8'), vars));
+  }
 }
 
 interface ApiStackConfigProps extends StackProps {
-    buildConfig: BuildConfig,
+  buildConfig: BuildConfig,
 }
